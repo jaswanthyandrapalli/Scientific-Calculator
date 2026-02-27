f55468f970f04510cb5b41d91f0cbc9288a45ab2
jaswanthyandrapalli <yandrapallijaswanth@gmail.com>
2026-02-27 21:04:21 +0530

    add keydown and toogle

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let display = document.querySelector('.res');
 let buttons = document.querySelectorAll('.keys button');
 let history_section = document.querySelector('.history');
 
+// Button Click Support
 buttons.forEach(button => {
     button.addEventListener("click", function () {
         let value = button.getAttribute("data-value");
@@ -24,7 +25,6 @@ function handleInput(value) {
             let result = eval(display.value);
             addToHistory(display.value + " = " + result);
             display.value = result;
-           
         } catch {
             display.value = "Error";
         }
@@ -53,12 +53,42 @@ function handleInput(value) {
     else {
         display.value += value;
     }
-   
 }
-
 
 function addToHistory(entry) {
     let li = document.createElement("li");
     li.textContent = entry;
     history_section.appendChild(li);
 }
+
+
+document.addEventListener("keydown", function (event) {
+
+    const keyMap = {
+        "Enter": "=",
+        "Backspace": "del",
+        "Escape": "c"
+    };
+
+    if (keyMap[event.key]) {
+        handleInput(keyMap[event.key]);
+    }
+
+    else if ("0123456789+-*/.".includes(event.key)) {
+        handleInput(event.key);
+    }
+});
+
+const toggleBtn = document.querySelector("#themeToggle");
+
+toggleBtn.addEventListener("click", function () {
+    document.body.classList.toggle("dark");
+   if(document.body.classList.contains("dark")){
+    toggleBtn.textContent = "☀️";
+    document.body.style.backgroundColor = "#333";
+    } else {
+    toggleBtn.textContent = "🌙";
+    document.body.style.backgroundColor = "#fff";
+
+   }
+});
